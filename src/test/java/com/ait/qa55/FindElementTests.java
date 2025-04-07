4b6b951cbb882fc33d1299733d2e4550a348e0d5
pataha <pataha@gmx.de>
2025-04-07 11:21:32 +0200

    Lesson 15

---
+++ src/test/java/com/ait/qa55/FindElementTests.java
@@ -0,0 +1,91 @@
+package com.ait.qa55;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FindElementTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://ilcarro.web.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void findElementByTagName() {
+        // find element by tag name
+        WebElement h1Element = driver.findElement(By.tagName("h1"));
+        System.out.println(h1Element.getText());
+        WebElement aElement = driver.findElement(By.tagName("a"));
+        System.out.println(aElement.getText());
+
+        //find list of elements by tag name
+        List<WebElement> aElements = driver.findElements(By.tagName("a"));
+        for (int i = 0; i < aElements.size(); i++) {
+            System.out.println(aElements.get(i).getText());
+
+        }
+        System.out.println(aElements.size());
+    }
+
+    @Test
+    public void findElementBySimpleLocators() {
+        // by id
+        driver.findElement(By.id("city"));
+
+        //by className
+        driver.findElement(By.className("header"));
+        System.out.println(driver.findElement(By.className("header")).getText());
+
+        //by linkText
+        driver.findElement(By.linkText("Let the car work"));
+
+        //by partialLinkText - chasticnoe sovpadenie
+        driver.findElement(By.partialLinkText("car"));
+    }
+
+    @Test
+    public void findElementByCssSelector() {
+        //tagName == css
+        //driver.findElement(By.tagName("h1"));
+        driver.findElement(By.cssSelector("h1"));
+
+        //id -> css(#)
+        //driver.findElement(By.id("city"));
+        driver.findElement(By.cssSelector("#city"));
+
+        //className -> css(.)
+        //driver.findElement(By.className("header"));
+        driver.findElement(By.cssSelector(".header"));
+
+        //[attr='value']
+        driver.findElement(By.cssSelector("[href='/registration?url=%2Fsearch']"));
+
+        //contains -> *
+        driver.findElement(By.cssSelector("[href*='/registration']"));
+
+        //start ->^
+        driver.findElement(By.cssSelector("[href^='/reg']"));
+
+        //end on ->$
+        driver.findElement(By.cssSelector("[href$='/search']"));
+    }
+
+}
+
